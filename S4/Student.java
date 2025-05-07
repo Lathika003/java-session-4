7bdd8f3de2182ae5cd54561fb6191999545d94d3
Lathika Nipun <145969120+Lathika003@users.noreply.github.com>
2025-05-07 20:20:53 +0530

    Add files via upload

---
+++ S4/Student.java
@@ -0,0 +1,37 @@
+class Student{
+	String name="Adam";
+	int marks=100;
+	
+	Student(){
+		name="NoName";
+	}
+	
+	Student(String n){
+	name=n;
+	}
+	
+	Student(String n, int m){
+		name=n;
+		marks=m;
+	}
+}
+
+class StudentDemo{
+	public static void main(String[] args){
+		System.out.println("Hi From StudentDemo Class");
+		System.out.println("Creating an Object of Student Class");
+		
+		
+		Student s1=new Student("Nice Name");
+		System.out.println("Name value of the object is " +s1.name);
+		System.out.println("Marks value of the object is " +s1.marks);
+		
+		s1.name="Bobi";
+		s1.marks=15;
+		System.out.println("Name value of the object is " +s1.name);
+		System.out.println("Marks value of the object is " +s1.marks);
+		
+		
+	}
+}
+
